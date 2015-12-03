5afa35abdc5a947a5dea045ee1774fbccf5d6426
Pat Yau <patrickyau@Pat-Yaus-MacBook-Pro.local>
2015-12-03 01:31:29 -0800

    added responsiveness to images and form elements, added hamburger menu icon

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,13 @@
+$('.hamburger-menu').hide();
 
+$('.menu-btn').click(function() {
+  $('.hamburger-menu').show();
+});
+
+$('#aboutTab').click(function() {
+  $('.hamburger-menu').hide();
+});
+
+$('#blogTab').click(function() {
+  $('.hamburger-menu').hide();
+});
