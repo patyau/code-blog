efa3bbc1917dc8917e9ced39516cd02baefb07c9
Pat Yau <patrickyau@Pat-Yaus-MacBook-Pro.local>
2015-12-16 14:20:26 -0800

    heroku updates

--- scripts/bio.js
+++ scripts/bio.js
@@ -2,17 +2,13 @@ var bio = {};
 bio.repos = [];
 bio.user;
 
-bio.requestGithubData = function(callback) {
-  bio.requestRepos();
-  bio.requestUser();
-}
-
 bio.requestRepos = function(callback) {
   $.ajax({
     type: 'GET',
     url: '/github.com/user/repos?sort=updated',
-  success: function(data, message, xhr) {
-    bio.repos = data;
+    success: function(data, message, xhr){
+      bio.repos = data;
+    }
   }).done(callback);
 };
 
@@ -20,8 +16,9 @@ bio.requestUser = function(callback) {
   $.ajax({
     type: 'GET',
     url: '/github.com/users/patyau',
-  success: function(data, message, xhr) {
-    console.log(data);
-    bio.user = data;
+    success: function(data, message, xhr){
+      console.log(data);
+      bio.user = data;
+    }
   }).done(callback);
 };
